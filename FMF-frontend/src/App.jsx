a60990c96851cb9087bb154524865a28f62cfe7d
Eli-Keli <muthokaelikeli@gmail.com>
2024-12-01 15:18:32 +0300

    Switch routing for GitHub pages

--- FMF-frontend/src/App.jsx
+++ FMF-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 
