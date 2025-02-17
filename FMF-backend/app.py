c6ba7aa594159644fb3be1ad165ae6c88ebd8122
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2025-02-17 20:52:49 +0300

    Update app.py

--- FMF-backend/app.py
+++ FMF-backend/app.py
@@ -5,4 +5,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port_value = os.environ.get("PORT", "10000")
+    app.run(debug=True, host="0.0.0.0", port=port_value)
